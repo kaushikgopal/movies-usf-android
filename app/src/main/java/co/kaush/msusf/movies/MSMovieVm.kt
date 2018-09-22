83cb639981681bff87cd0667295207eb6400522c
KG (ship) <iam.code@kaush.co>
2018-09-22 11:51:47 -0700

    fix: distince the view state

--- app/src/main/java/co/kaush/msusf/movies/MSMovieVm.kt
+++ app/src/main/java/co/kaush/msusf/movies/MSMovieVm.kt
@@ -78,7 +78,9 @@ class MSMainVm(
 
                 else -> throw RuntimeException("Unexpected result LCE state")
             }
-        }.doOnNext { viewState = it }
+        }
+            .distinct()
+            .doOnNext { viewState = it }
     }
 
     private fun results(events: Observable<out MSMovieEvent>): Observable<Lce<out MSMovieResult>> {
