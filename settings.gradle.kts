4d02c48ef0a5c5c62727877c9644a43d24d178f1
kaush <1042037+kaushikgopal@users.noreply.github.com>
2023-06-03 15:28:52 -0700

    migrate plugins to gradle version catalog

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,9 +9,12 @@ pluginManagement {
 dependencyResolutionManagement {
   repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
   repositories {
+    gradlePluginPortal()
     google()
     mavenCentral()
     maven("https://jitpack.io")
+    maven("https://oss.sonatype.org/content/repositories/snapshots")
+    mavenLocal()
   }
 }
 include(":app")
