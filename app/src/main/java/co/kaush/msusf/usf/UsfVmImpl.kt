de6d061ca27ff0ce38cf2b5d580eab5c8bb289aa
kaush <1042037+kaushikgopal@users.noreply.github.com>
2023-06-05 00:02:12 -0700

    UsfVmImpl - migrate to coroutines

--- app/src/main/java/co/kaush/msusf/usf/UsfVmImpl.kt
+++ app/src/main/java/co/kaush/msusf/usf/UsfVmImpl.kt
@@ -1,74 +1,97 @@
 package co.kaush.msusf.usf
 
-import io.reactivex.Observable
-import io.reactivex.ObservableTransformer
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.subjects.PublishSubject
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
 import timber.log.Timber
 
-class UsfVmImpl<E: Any, R: Any, VS: Any, VE: Any>(
-    eventToResultTransformer: ObservableTransformer<E, R>,
-    resultToViewStateTransformer: ObservableTransformer<R, VS>,
-    resultToViewEffectTransformer: ObservableTransformer<R, VE>,
-    logger: UsfVmLogger = object : UsfVmLogger {
-        override fun debug(message: String) = Timber.d(message)
-        override fun warning(message: String) = Timber.w(message)
-        override fun error(error: Throwable, message: String)  = Timber.e(error, message)
-    }
+@OptIn(ExperimentalCoroutinesApi::class)
+abstract class UsfVmImpl<E : Any, R : Any, VS : Any, VE : Any>(
+  private val viewModelScope: CoroutineScope,
+  private val processingDispatcher: CoroutineDispatcher = Dispatchers.IO,
+  logger: UsfVmLogger = object : UsfVmLogger {
+    override fun debug(message: String) = Timber.d(message)
+    override fun warning(message: String) = Timber.w(message)
+    override fun error(error: Throwable, message: String) = Timber.e(error, message)
+  }
 ) : UsfVm<E, R, VS, VE> {
 
-    override val eventSink: PublishSubject<E> = PublishSubject.create()
-    override val disposables: CompositeDisposable = CompositeDisposable()
+  // remember that a single E(vent) can yield multiple R(esult)s
+  //  so if you wish to show an initial loading state before the final result
+  //  emit multiple R(esult)s for the single E(vent)
+  abstract fun eventToResultFlow(): (E) -> Flow<R>
 
-    private val viewStateSink: Observable<VS>
-    private val viewEffectSink: Observable<VE>
+  // curiously, we don't return a Flow<VS> here
+  //  as every (R)esult will only ever be transformed into a single (V)iew(S)tate
+  //  if you want multiple (V)iew(S)tates, then emit multiple (R)esults
+  //  and transform each (R)esult accordingly to the respective (V)iew(S)tate
+  abstract fun resultToViewState(): (currentViewState: VS, result: R) -> VS
 
-    init {
-        logger.debug("------ init ${Thread.currentThread().name}")
+  // a single (R)esult can result in multiple (V)iew(E)ffects
+  //  for e.g. emit one VE for navigation and another for making an analytics call
+  //  hence a return type of Flow<VE>
+  abstract fun resultToViewEffectFlow(): (R) -> Flow<VE>
 
-        eventSink
-            .compose(eventToResultTransformer)
-            .doOnNext { logger.debug("----- result ${Thread.currentThread().name} $it") }
-
-            // share the result stream otherwise it will get subscribed to multiple times
-            // in the following also block
-            .share()
-
-            .also { result ->
-                 logger.debug("------ also ${Thread.currentThread().name}")
-                viewStateSink = result
-                    .compose(resultToViewStateTransformer)
-
-
-                    // if the viewState is identical
-                    // there's little reason to re-emit the same view state
-                    .distinctUntilChanged()
-
-                    .doOnNext { logger.debug("----- vs $it") }
-
-                    // when a screen rebinds to the ViewModel after rotation/config change
-                    // emit the last known viewState to new screen subscriber
-                    .replay(1)
+  abstract val initialViewState: VS
+  private val eventSink = MutableSharedFlow<E>()
+  private lateinit var viewStateSink: Flow<VS>
+  private lateinit var viewEffectSink: Flow<VE>
 
 
-                    // autoConnect makes sure the streams stays alive even when the UI disconnects
-                    // autoConnect(0) kicks off the stream without waiting for anyone to subscribe
-                    .autoConnect(0) { disposables.addAll(it)}
+  init {
+    logger.debug("------ init ${Thread.currentThread().name}")
 
+    viewModelScope.launch {
+      val resultFlow: Flow<R> = eventSink.flatMapLatest(transform = eventToResultFlow())
+          .onEach { logger.debug("----- result ${Thread.currentThread().name} $it") }
 
-                viewEffectSink = result
-                    .compose(resultToViewEffectTransformer)
-                    .doOnNext { logger.debug("----- ve $it") }
-            }
+      // share the result stream otherwise it will get subscribed to multiple times
+      // in the following also block
+      //  TODO: i'm not yet sure if we need this given we're using a mutable shared flow
+
+      withContext(processingDispatcher) {
+        viewStateSink = resultFlow
+            .scan(initialViewState, resultToViewState())
+
+            // if the viewState is identical
+            // there's little reason to re-emit the same view state
+            .distinctUntilChanged()
+
+            .onEach { logger.debug("----- vs $it") }
+
+            .shareIn(
+                // sharing is started within view model scope
+                viewModelScope,
+
+                // stream starts when first subscriber appears
+                // stops immediately when last subscriber disappears
+                // but keeps replay  forever
+                SharingStarted.WhileSubscribed(),
+
+                // keep the last view state in cache
+                // for new UI subscribers
+                replay = 1,
+            )
+
+        viewEffectSink = resultFlow.flatMapLatest(transform = resultToViewEffectFlow())
+            .onEach { logger.debug("----- ve $it") }
+      }
     }
 
-    override fun viewState(): Observable<VS> = viewStateSink
+  }
 
-    override fun viewEffect(): Observable<VE> = viewEffectSink
-
-    interface UsfVmLogger {
-        fun debug(message: String)
-        fun warning(message: String)
-        fun error(error: Throwable, message: String)
+  override fun processInput(event: E) {
+    viewModelScope.launch(processingDispatcher) {
+      eventSink.emit(event)
     }
+  }
+
+  override fun viewState(): Flow<VS> = viewStateSink
+
+  override fun viewEffect(): Flow<VE> = viewEffectSink
+
+  interface UsfVmLogger {
+    fun debug(message: String)
+    fun warning(message: String)
+    fun error(error: Throwable, message: String)
+  }
 }
