e647132d2e28efbf5746cddd79b09c7b478f9d89
kaush <1042037+kaushikgopal@users.noreply.github.com>
2023-06-05 00:04:44 -0700

    migrate MSMovieRepository to coroutines

--- app/src/main/java/co/kaush/msusf/movies/MSMovieRepository.kt
+++ app/src/main/java/co/kaush/msusf/movies/MSMovieRepository.kt
@@ -1,24 +1,22 @@
 package co.kaush.msusf.movies
 
 import com.google.gson.Gson
-import io.reactivex.Observable
-import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 @OpenClassOnDebug
 class MSMovieRepository @Inject constructor(
     val movieApi: MSMovieApi
 ) {
-    fun searchMovie(movieName: String): Observable<MSMovie?> {
-        return movieApi.searchMovie(movieName)
-            .map { response ->
-                response.body()?.let { return@map it }
-                response.errorBody()?.let { body ->
-                    return@map Gson().fromJson(
-                        body.string(),
-                        MSMovie::class.java
-                    )
-                }
-            }
+  suspend fun searchMovie(movieName: String): MSMovie? {
+    val response = movieApi.searchMovie(movieName)
+
+    response.body()?.let { return it }
+
+    return response.errorBody()?.let { body ->
+      Gson().fromJson(
+          body.string(),
+          MSMovie::class.java,
+      )
     }
+  }
 }
