becb2fcc21c0fe6ef30af230ae2156f156fdb1c6
kaush <1042037+kaushikgopal@users.noreply.github.com>
2023-06-06 15:43:11 -0700

    upgrade dagger + fix add source/target compatability

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,8 +40,10 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+      // after agp 8.1.0-alpha09, this is no longer needed
+      // https://kotlinlang.org/docs/gradle-configure-project.html#gradle-java-toolchains-support
+      sourceCompatibility = JavaVersion.VERSION_17
+      targetCompatibility = JavaVersion.VERSION_17
     }
 }
 
